ba20ba76994fc72903799ed3ac2c5ffdee531183
sungjin.yu <sungjin.yu@epapyrus.com>
2026-06-12 11:42:39 +0900

    Fine tune battery icon number position

--- BtBatteryBar.ps1
+++ BtBatteryBar.ps1
@@ -369,7 +369,7 @@ function New-BatteryImage {
             $fmt = New-Object System.Drawing.StringFormat
             $fmt.Alignment = [System.Drawing.StringAlignment]::Center
             $fmt.LineAlignment = [System.Drawing.StringAlignment]::Center
-            $textYOffset = [Math]::Max(1.0, [Math]::Round($H * 0.06, 1))
+            $textYOffset = [Math]::Max(0.5, [Math]::Round($H * 0.03, 1))
             $textRect = New-Object System.Drawing.RectangleF([float]$bx, [float]($by + $textYOffset), [float]$bw, [float]$bh)
             $g.DrawString([string]$Percent, $font, $textBrush, $textRect, $fmt)
             $fmt.Dispose(); $textBrush.Dispose(); $font.Dispose()
